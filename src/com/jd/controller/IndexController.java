6fe241ffffab86e0ba5b6152fc329e25cd08129a
EasonClark <1278239683@qq.com>
2017-08-18 18:09:22 +0800

    update jsp

--- src/com/jd/controller/IndexController.java
+++ src/com/jd/controller/IndexController.java
@@ -1,11 +1,13 @@
 package com.jd.controller;
 
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
 
 /**
@@ -19,13 +21,28 @@ import java.util.Date;
 @RequestMapping(value = "/")
 public class IndexController {
     private static final org.slf4j.Logger logger = LoggerFactory.getLogger(IndexController.class);
+    @Autowired
+    private HttpServletRequest request;
 
     @RequestMapping(value = "/index")
     public ModelAndView IndexController(){
         logger.debug("step into method IndexController");
+
+        String name = request.getParameter("name");
+        String password = request.getParameter("password");
+
         ModelAndView mav = new ModelAndView();
         mav.addObject("msg","Hello World");
         mav.getModelMap().put("time",new Date());
+
+        if (name != null && password != null) {
+            if("abc".equals(name) && "123".equals(password)){
+                mav.addObject("flag","true");
+            }else{
+                mav.addObject("flag","false");
+            }
+        }
+
         logger.debug("step out method IndexController");
         logger.info("execute method over");
         return mav;
--- web/WEB-INF/views/index.jsp
+++ web/WEB-INF/views/index.jsp
@@ -9,9 +9,36 @@
 <html>
   <head>
     <title>springmvc</title>
+      <script src="./js/jquery-3.2.1.js"></script>
+      <script type="text/javascript">
+        $("#login").click(function () {
+            var name = $("#name").val();
+            var password = $("#password").val();
+
+            if(name == null || name =="" || password == null || password == ""){
+                alert("name or password can't be null");
+                return;
+            }
+
+            $.ajax({
+                url : "/index",
+                type : "post",
+                data : {"name":name,"password":password},
+                success : function () {
+                    alert("login successfully");
+                }
+            });
+
+        });
+      </script>
   </head>
   <body>
   <p>msg:${msg}<br/></p>
   <p>current time:${time}<br/></p>
+
+  <input id="name" type="text" ><<br/>
+  <input id="password" type="password"><br/>
+  <input id="login" type="button" style="background: skyblue">
+
   </body>
 </html>
